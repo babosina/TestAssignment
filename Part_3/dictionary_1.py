2ad2a0b14195a315f264f77580f6d691d04aae07
astankevich <astankevich@astankevich>
2025-09-19 16:07:42 +0400

    add a second dictionary solution

--- Part_3/dictionary_1.py
+++ Part_3/dictionary_1.py
@@ -22,13 +22,16 @@ def get_s_key_val(i_dict: dict, keys: bool = True) -> list:
     Returns:
         List of keys if keys else List of values
     """
+    if not i_dict:
+        return []
     if keys:
         return sorted(i_dict.keys())
     return sorted(i_dict.values())
 
 
-x = {1: 'a', 4: 'b', 2: 'c', 100: 'e'}
+def test_get_s_key_val():
+    x = {1: 'a', 4: 'b', 2: 'c', 100: 'e'}
 
-assert get_s_key_val(x, True) == [1, 2, 4, 100]
-assert get_s_key_val(x, False) == ['a', 'b', 'c', 'e']
-assert get_s_key_val({}) == []
+    assert get_s_key_val(x, True) == [1, 2, 4, 100]
+    assert get_s_key_val(x, False) == ['a', 'b', 'c', 'e']
+    assert get_s_key_val({}) == []
---
+++ Part_3/dictionary_2.py
@@ -0,0 +1,34 @@
+"""
+The function should take two dictionaries as parameters (with default values) and return two tuples:
+one containing the matching values and another containing the matching keys.
+If one of the dictionaries is not provided, it should be assumed that all values match.
+The tuples must be sorted.
+
+def find_dupes('''define'''):
+    pass
+
+y = {1: 'a', 4: 'b', 2: 'c', 100: 'e'}
+z = {2: 'a', 4: 'b', 9: 'c', 101: 'd'}
+assert find_dupes(y, z) == (('a','b','c'), (2,))
+assert find_dupes(y) == (('a','b','c','e'), (1,2,4,100))
+"""
+
+
+def find_dupes(x: dict = None, y: dict = None) -> tuple:
+    x = x or {}
+    y = y or {}
+    if not x or not y:
+        return (tuple(sorted(set(x.values()) | set(y.values()))),
+                tuple(sorted(set(x.keys() | set(y.keys())))))
+
+    return (tuple(sorted((set(x.values()) & set(y.values())))),
+            tuple(sorted((set(x.keys()) & set(y.keys())))))
+
+
+def test_find_dupes():
+    y = {1: 'a', 4: 'b', 2: 'c', 100: 'e'}
+    z = {2: 'a', 4: 'b', 9: 'c', 101: 'd'}
+
+    assert find_dupes(y, z) == (('a', 'b', 'c'), (2, 4))
+    assert find_dupes(y) == (('a', 'b', 'c', 'e'), (1, 2, 4, 100))
+    assert find_dupes() == ((), ())
